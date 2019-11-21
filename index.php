6ca630abe9a1cd372f6e87f9cfe477b1a9027108
Dylan <dylngae@outlook.com>
2019-11-21 16:55:30 +0100

    header

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php include('public/header.php'); ?>
 
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 jLogo">
+                <img src="assets/style/img/logo.png" width="60%" />
+            </div>
+            <div class="col-6">
+                <div class="jumbotron mt-5 mb-5 text-center">
+                    <h1>Bienvenue sur Api3D</h1>
+                    <hr width='50%'>
+                    <p>
+                        Api3D est un site de référencement pour les imprimeurs 3D. <br />
+                        Ici, vous trouverez un espace communautaire ainsi qu'un espace personnel <br />
+                        vous permettant de réaliser un tableau de vos consomations de bobine de fil.
+                    </p>
+                </div>
+            </div>
+        </div>
 
+    </div>
+</header>
 
 <?php include('public/footer.php'); ?>
--- public/header.php
+++ public/header.php
@@ -15,3 +15,29 @@
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand font-weight-bold page-title" href="#">
+                <img src="assets/style/img/logo.png" />
+                API3D
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
