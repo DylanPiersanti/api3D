fde83faeb18a13029a02fbb5398db3c052189c56
Dylan <dylngae@outlook.com>
2019-11-22 11:00:36 +0100

    nextStep

--- index.php
+++ index.php
@@ -1,25 +1,74 @@
 <?php include('public/header.php'); ?>
 
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 jLogo">
-                <img src="assets/style/img/logo.png" width="60%" />
-            </div>
-            <div class="col-6">
-                <div class="jumbotron mt-5 mb-5 text-center">
-                    <h1>Bienvenue sur Api3D</h1>
-                    <hr width='50%'>
-                    <p>
-                        Api3D est un site de référencement pour les imprimeurs 3D. <br />
-                        Ici, vous trouverez un espace communautaire ainsi qu'un espace personnel <br />
-                        vous permettant de réaliser un tableau de vos consomations de bobine de fil.
-                    </p>
+<header class="header">
+    <div class="headerTitle">
+        <div>
+            <h1>API3D</h1>
+            <p>Bienvenue sur API3D, l'application web des imprimeurs 3D</p>
+        </div>
+    </div>
+</header>
+
+<main class="container mt-4">
+    <div class="jumbotron">
+
+        <h1 class="h1-reponsive mb-3 blue-text"><strong>Hello, world!</strong></h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+            to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger
+            container.
+        </p>
+        <p class="lead">
+            <a class="btn btn-cyan btn-lg" href="#" role="button">Learn more</a>
+        </p>
+
+    </div>
+    <div class="row display-flex">
+        <div class="col-6">
+            <div class="card" style="width: 100%;">
+                <img src="assets/style/img/general.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
-
+        <div class="col-6">
+            <div class="card" style="width: 100%;">
+                <img src="assets/style/img/machines.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
-</header>
+
+    <div class="row mt-4">
+        <div class="col-6">
+            <div class="card" style="width: 100%;">
+                <img src="assets/style/img/techno.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card" style="width: 100%;">
+                <img src="assets/style/img/projects.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php include('public/footer.php'); ?>
